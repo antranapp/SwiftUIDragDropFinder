6d8cec0f074c1a2e48d794e2af264b29b1c43bdc
An Tran <tran.binhan@gmail.com>
2024-05-25 21:12:31 +0700

    Initial Commit

---
+++ FileDragDrop/FileDragDropApp.swift
@@ -0,0 +1,17 @@
+//
+//  FileDragDropApp.swift
+//  FileDragDrop
+//
+//  Created by An Tran on 25/5/24.
+//
+
+import SwiftUI
+
+@main
+struct FileDragDropApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
